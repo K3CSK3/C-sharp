21be6c3a3df7159c8a513742a8e1e4496cf04b66
Wittner Attila <wiattila8@gmail.com>
2023-09-08 08:30:38 +0200

    Ora-09-08

---
+++ 00_Változók/Solution_valtozok/feladat_00/Program.cs
@@ -0,0 +1,13 @@
+﻿string name = "Katarzyna Skowronsa-Dolata";
+string nationality = "Lengyel";
+int yearOfBirth = 1983;
+string placeOfBirth = "Warszawa";
+double height = 1.89;
+int weight = 75;
+int hitHeight = 317;
+int rampartHeight = 302;
+int dressNumber = 2;
+bool teamCaptain = true;
+char captainSymbol = 'C';
+
+Console.ReadKey();
