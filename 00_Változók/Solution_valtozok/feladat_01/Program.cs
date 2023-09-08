56984c2e86c5c79f477cf8b6f1bb9bd5a6fed61b
Wittner Attila <wiattila8@gmail.com>
2023-09-08 09:27:43 +0200

    Ora-09-08

---
+++ 00_Változók/Solution_valtozok/feladat_01/Program.cs
@@ -0,0 +1,4 @@
+﻿int productionYear = 1970;
+string carModell = "Torino Cobra SportsRoof";
+
+Console.WriteLine($"A Ford autógyár {productionYear}-ben gyártotta le a  {carModell} modellt.");
---
+++ 00_Változók/Solution_valtozok/feladat_02/Program.cs
@@ -0,0 +1,5 @@
+﻿string series = "i7";
+int type = 8700;
+double clockrate = 4.6;
+
+Console.WriteLine($"Az INTEL {series} sorozatjelzésű mikroprocesszor {type} típusának munkafrekvenciája {clockrate}GHz");
