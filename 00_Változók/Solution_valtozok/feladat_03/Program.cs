87bf32febebaa1cc68197be0fffe931577128897
Wittner Attila <wiattila8@gmail.com>
2023-09-08 09:37:04 +0200

    Ora-09-08

---
+++ 00_Változók/Solution_valtozok/feladat_03/Program.cs
@@ -0,0 +1,7 @@
+﻿string dynasty = "Ming";
+int startYear = 1368;
+int finishYear = 1644;
+string structureName = "Kínai Nagyfal";
+int structureLength = 7200;
+
+Console.WriteLine($"A {dynasty} dinasztia {startYear} és {finishYear} közt építette a ma látható {structureName} {structureLength}km hosszú szakaszát");
---
+++ 00_Változók/Solution_valtozok/feladat_04/Program.cs
@@ -0,0 +1,8 @@
+﻿int year = 2008;
+string creator = "Sukhoj";
+string type = "SU-35S";
+double speed = 2.4;
+string speedType = "m/s";
+
+
+Console.WriteLine($"Oroszország legmodernebb vadászrepülőgépe a {year}-ban bemutatott {creator} gyártotta {type} amely képes a {speed} Mach sebességre, ami 720{speedType}-nak felel meg");
