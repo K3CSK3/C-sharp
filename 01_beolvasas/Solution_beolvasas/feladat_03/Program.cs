1d4d45ff974813adadae834b6f0dea48e3f80c51
Wittner Attila <wiattila8@gmail.com>
2023-09-12 09:03:38 +0200

    Ora-09-12

---
+++ 01_beolvasas/Solution_beolvasas/feladat_03/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+Console.Write("Please type your name: ");
+string name = Console.ReadLine();
+
+Console.Write("Please type your height (in meters): ");
+double height = double.Parse(Console.ReadLine(), new CultureInfo("en-EN"));
+
+Console.Write($"{name} az ön magassága {height}m!");
