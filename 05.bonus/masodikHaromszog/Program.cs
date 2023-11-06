37d7f75a1eec39f3dc43f31b37e9f7b88aa03583
K3CSK3 <114721860+K3CSK3@users.noreply.github.com>
2023-11-06 16:52:11 +0100

    Otthon-11-06

---
+++ 05.bonus/masodikHaromszog/Program.cs
@@ -0,0 +1,21 @@
+﻿int stepNumber;
+bool Isnumber = false;
+do
+{
+    Console.Write("Please type a step amount: ");
+    string input = Console.ReadLine();
+
+    Isnumber = int.TryParse(input, out stepNumber);
+}
+while (!Isnumber || stepNumber < 1);
+
+Console.Clear();
+
+for (int i = stepNumber; i >= 1; i--)
+{
+    for (int j = i; j >= 1; j--)
+    {
+        Console.Write($"{j} \t");
+    }
+    Console.WriteLine();
+}
