4877d04741e5b313245fe918dffd2a27eac58c13
Wittner Attila <wiattila8@gmail.com>
2023-11-07 09:33:42 +0100

    Ora-11-07

---
+++ 11_Függvények/Solution_fuggvenyek/Feladat_00/Program.cs
@@ -0,0 +1,11 @@
+﻿using IOLibrary;
+
+Console.WriteLine("Please type a number: ");
+int number1 = ExtendedConsole.ReadInteger();
+
+Console.WriteLine("Please type a floating point number: ");
+double number2 = ExtendedConsole.ReadDouble();
+
+number1.WriteToConsole();
+
+number2.WriteToConsole();
---
+++ 11_Függvények/Solution_fuggvenyek/IOLibrary/ExtendedConsole.cs
@@ -0,0 +1,33 @@
+﻿namespace IOLibrary;
+
+
+public static class ExtendedConsole
+{
+    public static int ReadInteger()
+    {
+        bool isNumber;
+        int number;
+
+        do {
+            string text = Console.ReadLine();
+            isNumber = int.TryParse(text, out number);
+        } while (!isNumber);
+
+        return number;
+    }
+
+    public static double ReadDouble()
+    {
+        bool isNumber;
+        double number;
+
+        do
+        {
+            string text = Console.ReadLine();
+            isNumber = double.TryParse(text, out number);
+        } while (!isNumber);
+
+        return number;
+    }
+
+}
