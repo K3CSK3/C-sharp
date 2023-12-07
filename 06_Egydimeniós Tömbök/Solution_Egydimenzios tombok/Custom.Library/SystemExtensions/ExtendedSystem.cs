fb29bc4ab2a8f2198e43e2610d2d1e2f93c5fca0
Wittner Attila <wiattila8@gmail.com>
2023-12-07 09:38:06 +0100

    Ora-12-07

---
+++ 06_Egydimeniós Tömbök/Solution_Egydimenzios tombok/Custom.Library/SystemExtensions/ExtendedSystem.cs
@@ -0,0 +1,61 @@
+﻿namespace System;
+
+public static class ExtendedSystem
+{
+    public static void WriteToConsole(this object number)
+    {
+        Console.WriteLine(number);
+    }
+
+    public static int Summary(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public static int Difference(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+
+    public static int Product(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+
+    public static double Division(double num1, double num2)
+    {
+        return num1 / num2;
+    }
+
+    public static void ColoredText(string text)
+    {
+        int lenght = text.Length;
+        while (lenght > 15)
+        {
+            lenght -= 15;
+        }
+        Console.ForegroundColor = (ConsoleColor)lenght;
+        Console.Write(text);
+        Console.ResetColor();
+    }
+
+    public static double ConvertToEUR(double baseMoney, string convType)
+    {
+        if (convType == "JPY") 
+        {
+            return baseMoney * 0.0061;
+        }
+        else if (convType == "USD") 
+        {
+            return baseMoney * 0.92;
+        }
+        else if (convType == "CHF") 
+        {
+            return baseMoney * 1.04;
+        }
+        else
+        {
+            return baseMoney;
+        }
+    }
+}
